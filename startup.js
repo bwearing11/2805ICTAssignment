64c589b04e7c14aca4f48d1c93fc98058a40bba7
gerardegan <110706026+gerardegan@users.noreply.github.com>
2022-08-23 00:08:35 +1000

    Add files via upload

--- startup.js
+++ startup.js
@@ -17,11 +17,18 @@ var game = new Phaser.Game(config);
 function preload(){
     this.load.image('bgimage', 'Assets/bgimage3.png');
     this.load.image('logo_image', 'Assets/title3.png');
+    this.load.image('student', 'Assets/students.png');
+    this.load.image('button', 'Assets/button.png');
 }
 
 function create(){
     this.add.image(0, 0, 'bgimage').setOrigin(0,0);
-    this.add.image(384, 63, 'logo_image').setOrigin(0,0);
+    this.add.image(480, 63, 'logo_image').setOrigin(0,0);
+    this.add.image(30, 30, 'student').setOrigin(0,0);
+    this.add.image(256, 447, 'button').setOrigin(0,0);
+    this.add.image(480, 447, 'button').setOrigin(0,0);
+    this.add.image(704, 447, 'button').setOrigin(0,0);
+    this.add.image(926, 447, 'button').setOrigin(0,0);
 }
 
 function update (){
