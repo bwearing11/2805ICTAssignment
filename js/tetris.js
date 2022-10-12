f03636a99939378ac40aaa2e7f299d17ff0c0973
ZoeWaters21 <110453020+ZoeWaters21@users.noreply.github.com>
2022-10-12 10:12:28 +1000

    fixed positional matrix

--- js/tetris.js
+++ js/tetris.js
@@ -17,6 +17,7 @@ preload () // preloads all the assets at games initialisation
     this.load.image('next', 'Assets/next.png');
     this.load.image('group', 'Assets/group20.png');
     this.load.image('borderblock', 'Assets/borderblock.png');
+    this.load.image('testblock', 'Assets/borderblock.png');
     this.load.image('leftlblock', 'Assets/leftlblockfull.png');
     this.load.image('leftzigblock', 'Assets/leftzigblockfull.png');
     this.load.image('rightlblock', 'Assets/rightlblockfull.png');
@@ -61,6 +62,7 @@ create ()//the game engines create function, it runs static code that doesnt req
             fieldArray.push(column);
         }
     }
+    
 
     let createGamefield = (x, y) => {//creates a border around the gamefield with grey blocks
         for (var i = 0; i < fieldArray.length; i++){
@@ -72,6 +74,22 @@ create ()//the game engines create function, it runs static code that doesnt req
             }
             for (var j = 0; j < gameSizeWidth+1; j++){
                 this.add.image(x+(j*blockSize),y+((gameSizeHeight+3)*blockSize),'borderblock');
+                fieldArray[gameSizeHeight+3][j] = 1    
+            }
+        }
+    }
+    let createTestGamefield = (x, y) => {//creates a border around the gamefield with grey blocks
+        for (var i = 0; i < fieldArray.length; i++){
+            fieldArray[i][0] = 1
+            fieldArray[i][gameSizeWidth + 2] = 1
+            if (i > 2){
+                this.add.image(x, y + (i*(blockSize*0.25)), 'borderblock').setScale(0.25);
+                this.add.image(x+(gameSizeWidth*(blockSize*0.25)), y + (i*(blockSize*0.25)), 'borderblock').setScale(0.25);
+
+            }
+            for (var j = 0; j < gameSizeWidth+1; j++){
+                this.add.image(x+(j*(blockSize*0.25)),y+((gameSizeHeight+3)*(blockSize*0.25)),'borderblock').setScale(0.25);
+                
                 fieldArray[gameSizeHeight+3][j] = 1    
             }
         }
@@ -120,6 +138,7 @@ create ()//the game engines create function, it runs static code that doesnt req
     createGUI()
     createFieldArray()//
     createGamefield(fieldCords[0], fieldCords[1])
+    //createTestGamefield(950, 750)
     createTetromino(currentPiece, pieceStartPoint[0], pieceStartPoint[1]);
     nextPieceImage = this.add.image(fieldCords[0]+gameSizeWidth*blockSize+225, fieldCords[1]+(blockSize*2.5+90), blockTextures[nextPiece])
     controls = this.input.keyboard.createCursorKeys();//variable to hold the array of available interaction keys
@@ -185,7 +204,7 @@ update ()//updates the game engine at 60 fps
 
     if (this.input.keyboard.checkDown(controls.left, 250))//moves the tetromino left when the left arrow key is pressed, with a 250ms delay inbetween
     {
-  
+        canMove = false
         for (var i = 0; i < currentPosition.length; i++){//checks if there is an obstacle and stops the input if there is
             var nextPosX = fieldArray[(currentPosition[i][1])][(currentPosition[i][0])-2];
 
@@ -199,18 +218,21 @@ update ()//updates the game engine at 60 fps
         }
 
         if (canMove && !pauseState){
-            
+            canMove = false
             for (var i = 0; i < currentPosition.length; i++){//moves the block to the left and updates the current position
                 currentPosition[i][0] -= 1;
                 console.log(currentPosition)
             }
             tetrominoVelocity.x -= 32
+            canMove = true
         }
+        canMove = true
     }
         
         
     else if (this.input.keyboard.checkDown(controls.right, 250))//moves the tetromino right when the right arrow is pressed, with a 250ms delay inbetween
     {
+        canMove = false
         for (var i = 0; i < currentPosition.length; i++){//checks if there is an obstacle and stops the input if there is
             var nextPosX = fieldArray[(currentPosition[i][1])][(currentPosition[i][0])+2];
 
@@ -224,13 +246,15 @@ update ()//updates the game engine at 60 fps
         }
 
         if (canMove && !pauseState){//moves the block to the right and updates the current position
+            canMove = false
             for (var i = 0; i < currentPosition.length; i++){
                 currentPosition[i][0] += 1; 
                 console.log(currentPosition)  
             }
             tetrominoVelocity.x += 32
+            canMove = true
         }
-
+        canMove = true
     }
 
     else if (this.input.keyboard.checkDown(controls.up, 450)){//rotates the block when the up arrow is pressed, with a 450ms delay inbetween
@@ -238,6 +262,7 @@ update ()//updates the game engine at 60 fps
         if(r270){//checks which rotation stage the piece is on, listed backwards to avoid it triggering twice when the state changes
             var newXPos = [];
             var newYPos = [];
+            canMove = false;
             for (var i = 0; i < blockPosition[currentPiece].length; i++){//takes away the previous matrix position and adds the next rotation positions 
                 var iterRotX = currentPosition[i][0] - rotation3[currentPiece][i][0];
                 var iterRotY = currentPosition[i][1] - rotation3[currentPiece][i][1];
@@ -252,9 +277,9 @@ update ()//updates the game engine at 60 fps
                     break;
                 }
                 else{
-                    canMove = true;
                     r270 = false;//changes rotation state
-                    r0 = true;                    
+                    r0 = true; 
+                    canMove = true;                   
                 }   
             }
         }
@@ -263,6 +288,7 @@ update ()//updates the game engine at 60 fps
                         //Note: reduce redundancy
             var newXPos = [];
             var newYPos = [];
+            canMove = false
             for (var i = 0; i < blockPosition[currentPiece].length; i++){//takes away the previous matrix position and adds the next rotation positions
                 var iterRotX = currentPosition[i][0] - rotation2[currentPiece][i][0];
                 var iterRotY = currentPosition[i][1] - rotation2[currentPiece][i][1];
@@ -277,15 +303,16 @@ update ()//updates the game engine at 60 fps
                     break;
                 }
                 else{
-                    canMove = true;
                     r180 = false;//changes rotation state
                     r270 = true;
+                    canMove = true;
                 }   
             }
         }
         else if(r90){//checks which rotation stage the piece is on, listed backwards to avoid it triggering twice when the state changes
             var newXPos = [];
             var newYPos = [];
+            canMove = false;
             for (var i = 0; i < blockPosition[currentPiece].length; i++){//takes away the previous matrix position and adds the next rotation positions
                 var iterRotX = currentPosition[i][0] - rotation1[currentPiece][i][0];
                 var iterRotY = currentPosition[i][1] - rotation1[currentPiece][i][1];
@@ -300,21 +327,22 @@ update ()//updates the game engine at 60 fps
                     break;
                 }
                 else{
-                    canMove = true;
                     r90 = false;//changes rotation state
-                    r180 = true;                    
+                    r180 = true; 
+                    canMove = true;                  
                 }   
             } 
         }
         else if(r0){//checks which rotation stage the piece is on, listed backwards to avoid it triggering twice when the state changes
             var newXPos = [];
             var newYPos = [];
+            canMove = false
             for (var i = 0; i < blockPosition[currentPiece].length; i++){//takes away the previous matrix position and adds the next rotation positions
                 var iterRotX = currentPosition[i][0] - blockPosition[currentPiece][i][0];
                 var iterRotY = currentPosition[i][1] - blockPosition[currentPiece][i][1];
                 newXPos.push(iterRotX + rotation1[currentPiece][i][0]);
                 newYPos.push(iterRotY + rotation1[currentPiece][i][1]);
-            } 
+            }            
 
             for (var i = 0; i < blockPosition[currentPiece].length; i++){//makes sure there is room to rotate
                 if (fieldArray[newYPos[i]][newXPos[i]] == 1) {
@@ -323,9 +351,9 @@ update ()//updates the game engine at 60 fps
                     break;
                 }
                 else{
-                    canMove = true;
                     r0 = false;//changes rotation state
-                    r90 = true;                    
+                    r90 = true; 
+                    canMove = true;                   
                 }   
             }         
         }
@@ -333,17 +361,19 @@ update ()//updates the game engine at 60 fps
 
         
         if (canMove && !pauseState){//rotates the tetris piece clockwise and updates the matrix positions
-
-            this.tetromino.angle += 90;
+            canMove = false
             this.time.addEvent(timedEvent)
             currentPosition = [];
                 for (var i = 0; i < blockPosition[currentPiece].length; i++){
                         currentPosition.push([newXPos[i], newYPos[i]]);
                 } 
-            console.log(currentPosition) //debug comment to check the rotation calculations are correct       
+            this.tetromino.angle += 90;
+            canMove = true     
         }
-
-        }else if (this.input.keyboard.checkDown(controls.down, 150)){//moves the piece down when the down arrow is pressed, with a delay of 150ms in between
+       
+        }
+        
+        else if(this.input.keyboard.checkDown(controls.down, 150)){//moves the piece down when the down arrow is pressed, with a delay of 150ms in between
             for (var i = 0; i < currentPosition.length; i++){//updates matrix position
                 var nextPosY = fieldArray[(currentPosition[i][1])+1][(currentPosition[i][0])];
 
@@ -357,12 +387,14 @@ update ()//updates the game engine at 60 fps
         }
 
         if (canMove && !pauseState){
+            canMove = false
             for (var i = 0; i < currentPosition.length; i++){//moves the piece down and updates matrix
                 this.time.addEvent(timedEvent);
                 currentPosition[i][1] += 1;
                 console.log(currentPosition)
             }
             tetrominoVelocity.y += 32
+            canMove = true
         }
     }
     this.tetromino.setVelocity(tetrominoVelocity.x, tetrominoVelocity.y);//tells the physics engine the tetris piece will travel on 2 axis
@@ -373,8 +405,10 @@ update ()//updates the game engine at 60 fps
         for (var i = 0; i < blockPosition[currentPiece].length; i++){//saves the current position into the field array, making it an obstacle and part of the game field
             fieldArray[currentPosition[i][1]][currentPosition[i][0]] = 1;
         }
-        console.log(fieldArray);//debug comment to check the block was saved in the right position
-
+        r0 = true
+        r90 = false
+        r180 = false
+        r270 = false
         //the following section is a copy of the createTetromino() function, requires a global function or class but current issues with scope
         var x = pieceStartPoint[0];
         var y = pieceStartPoint[1];
@@ -391,6 +425,7 @@ update ()//updates the game engine at 60 fps
             var iterBlockPosY = blockPosition[nextPiece][i][1]+3;
             currentPosition.push([iterBlockPosX,iterBlockPosY]);
         }
+        
 
         //replace the next piece display, Note: needs function
         nextPieceImage.destroy();
@@ -403,8 +438,16 @@ update ()//updates the game engine at 60 fps
 
 
     }
-
+/*
+    for (var i = 0; i < fieldArray.length; i++){
+        for (var j = 0; j < gameSizeWidth+1; j++){
+            if (fieldArray[i][j] == 1)
+                this.add.image(900+(j*(blockSize*0.25)), 750+(i*(blockSize*0.25)), 'borderblock').setScale(0.25);
+        }
+    }
+*/
 }
+
 }
 
 function onEvent()//function for timer processing
@@ -424,12 +467,14 @@ function onEvent()//function for timer processing
     }
 
     if (canMove && !pauseState){// if the piece can move and the timer runs out, move down a space
+        canMove = false
         this.time.addEvent(timedEvent);
         for (var i = 0; i < currentPosition.length; i++){
             currentPosition[i][1] += 1;
             console.log(currentPosition)
         }
         tetrominoVelocity.y += 32
+        canMove = true
     }
     this.tetromino.setVelocity(tetrominoVelocity.x, tetrominoVelocity.y);//same as in update(), just reinitalisting to keep in scope
 }
